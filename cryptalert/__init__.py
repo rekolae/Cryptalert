1fa438d99802bd4ef9e1f48e03fd5da5e99868cf
Emil Rekola <emil.rekola@hotmail.com>
2021-02-20 21:10:28 +0200

    [1-config] Added docstrings

--- cryptalert/__init__.py
+++ cryptalert/__init__.py
@@ -1,2 +1,11 @@
+"""
+Main entry point for the application
+
+Emil Rekola <emil.rekola@hotmail.com>, 2021
+"""
+
 from pathlib import Path
+
+
+# Get application name from the project folder name
 prog_name = Path(__file__).parent.parent.name
