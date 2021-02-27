05e7652ac6c9b8901f0106d2fbc22d67ff7843d1
Emil Rekola <emil.rekola@hotmail.com>
2021-02-27 23:25:01 +0200

    [2-basic-ui] Added empty class frame for text ui

---
+++ cryptalert/text_ui/tui.py
@@ -0,0 +1,23 @@
+"""
+Handles showing the fetched data on a simple TUI
+
+Emil Rekola <emil.rekola@hotmail.com>, 2021
+"""
+
+
+class TUI:
+    """
+    Class for displaying a simple text UI
+    """
+
+    def __init__(self):
+        pass
+
+    def init_curses(self):
+        pass
+
+    def init_colors(self):
+        pass
+
+    def update_data_windows(self):
+        pass
