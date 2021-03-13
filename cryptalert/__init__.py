810c3dd0ec801fa8cc81d224da62c90c032aa6ce
Emil Rekola <emil.rekola@hotmail.com>
2021-03-13 23:26:39 +0200

    [6-combine-parts] Removed unnecessary config import from __init__.py

--- cryptalert/__init__.py
+++ cryptalert/__init__.py
@@ -7,9 +7,6 @@ Emil Rekola <emil.rekola@hotmail.com>, 2021
 # STD imports
 from pathlib import Path
 
-# Local imports
-from cryptalert.config import Config
-
 
 # Get application name from the project folder name
 prog_name = Path(__file__).parent.parent.name
